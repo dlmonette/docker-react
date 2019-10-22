e2863b715755852cb1d08e2211c296b5751d6df5
David Langlois-Monette <David.Langlois-Monette@trilliant.com>
2019-10-22 16:51:08 -0400

    feature change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to matrix neo neo neo reloaded.
+          Edit <code>src/App.js</code> Le temps ne respecte pas ce qui se fait sans lui.
         </p>
         <a
           className="App-link"
